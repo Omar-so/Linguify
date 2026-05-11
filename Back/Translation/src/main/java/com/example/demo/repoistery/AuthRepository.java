c2fceba971258ba9fe798f6823413d7e0eafcc00
Omar-so <168472388+Omar-so@users.noreply.github.com>
2026-05-11 10:18:02 -0400

    Delete Back directory

--- Back/Translation/src/main/java/com/example/demo/repoistery/AuthRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repoistery;
-import com.example.demo.Entitty.Users;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-@Repository
-public interface AuthRepository extends JpaRepository<Users, Long> {
-    Users findByUsername(String username);
-
-}
